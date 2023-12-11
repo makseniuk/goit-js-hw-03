4ef7dc2ccab3278e94444553c062b45759bc270b
makseniuk <144346873+makseniuk@users.noreply.github.com>
2023-12-11 19:42:05 +0100

    hm3.1

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function filterArray(numbers, value) {
+
+  let filteredArray = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      filteredArray.push(numbers[i]);
+    }
+  }
+
+  return filteredArray;
+}
